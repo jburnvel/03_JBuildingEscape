42f5a4340820467e660b412f011c9f428a6b1a93
jburnvel <jburnvel@users.noreply.github.com>
2017-07-19 10:04:54 -0500

    update last version scripts

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,7 +1,10 @@
 // Copyrigth Jorge Burnes 2017
 
 #include "OpenDoor.h"
-
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+ 
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -17,18 +20,45 @@ UOpenDoor::UOpenDoor()
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
-	Super::BeginPlay();
+	Super::BeginPlay();	 
 
-	// ...
-	
+	//Find the owning Actor
+	Owner = GetOwner();
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	// Find the owning objet
+	FString ObjectName = Owner->GetName();
+	FQuat ObjectRot = Owner->GetTransform().GetRotation();//"X=algo, Y=algo etc..."; 	
+	UE_LOG(LogTemp, Warning, TEXT("%s is Rotation  X=%f, Y=%f, Z=%f"), *ObjectName, ObjectRot.X, ObjectRot.Y, ObjectRot.Z);
 }
 
+// Open Door by angle set in OpenAngle
+void UOpenDoor::OpenDoor()
+{ 
+	Owner->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
+}
+
+// Close Door by angle set in OpenAngle
+void UOpenDoor::CloseDoor()
+{ 
+	Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
+}
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+	// Poll the Trigger Volume
+	// If the ActorThatOpens is in the volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+	}
+	//Check if it's time to colse the door
+	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay)
+	{
+		CloseDoor();
+	} 
 }
-
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -15,15 +16,32 @@ class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UOpenDoor();
+	
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	void OpenDoor();
+	void CloseDoor();
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
 	
+
+private:
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 90.0f;	
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	float DoorCloseDelay = 1.f;
+
+	float LastDoorOpenTime;
+
+	AActor* ActorThatOpens; // Remember pawn inherits from actor
+	AActor* Owner; //  The owning door
 };
